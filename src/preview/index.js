188c55eec43c2292dffd5e4e3ab7bf66958b561b
Alex Shyba <alex.shyba@uniform.dev>
2025-03-06 13:22:08 -0800

    chore: remove preview path

--- src/preview/index.js
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { EMPTY_COMPOSITION } from "@uniformdev/canvas";
-
-const container = document.getElementById("app");
-const root = createRoot(container);
-root.render(<UniformComposition data={EMPTY_COMPOSITION} />);
