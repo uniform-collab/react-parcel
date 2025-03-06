ffd01fe81e89132c8725c575e7af513faf3fdd72
Alex Shyba <alex.shyba@uniform.dev>
2025-03-06 13:20:56 -0800

    feat: dynamic composition path resolution

--- src/Root.js
+++ src/Root.js
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
 import { UniformComposition } from "@uniformdev/canvas-react";
+import { EMPTY_COMPOSITION } from "@uniformdev/canvas";
 import { resolveComponent } from "./lib/uniform/resolve";
 
 export function Root() {
   let [composition, setComposition] = useState(null);
-  const compositionPath = "/campaign-emails/product-launch-q2-25";
   const projectId = "1d90b3f7-0fe8-4157-88dd-6d9cbe737c46";
+
   useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    let path = params.get("path"); // composition path
     fetch(
-      `https://uniform.app/api/v1/route?path=${compositionPath}&projectId=${projectId}`,
+      `https://uniform.app/api/v1/route?path=${path}&projectId=${projectId}`,
       {
         headers: {
           "x-api-key":
@@ -24,7 +27,7 @@ export function Root() {
 
   return (
     <UniformComposition
-      data={composition}
+      data={composition ?? EMPTY_COMPOSITION}
       resolveRenderer={(props) => resolveComponent(props)?.component}
     />
   );
